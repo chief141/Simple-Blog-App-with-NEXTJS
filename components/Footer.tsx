f13c4912f5fc978aa69684dbe363531820752ab1
Prashant <prashantrahul141@protonmail.com>
2022-10-29 00:16:44 +0530

    added about us page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
 import styles from '../styles/components_styles/footer.module.css';
 
 const CopyFooter = () => {
-  return <h6 className={styles.copyrightFooter}>copyright @ chief141</h6>;
+  return (
+    <>
+      <h6 className={styles.copyrightFooter}>
+        ©Copyright 2022 Chief. All rights reserved.
+      </h6>
+    </>
+  );
 };
 
 export default CopyFooter;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import NavBar from '../components/navbar';
+import CopyFooter from '../components/Footer';
+import styles from '../styles/basicroutes_styles/about.module.css';
 
 const AboutUs = () => {
   return (
@@ -11,8 +13,22 @@ const AboutUs = () => {
       </Head>
       <main></main>
       <NavBar></NavBar>
+      <div className={styles.about_main}>
+        <h2 className={styles.about_us_title}>About us?</h2>
+        <div className={styles.about_content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id quae magni
+          at sint quasi ipsam ullam nostrum voluptatum. Adipisci alias beatae
+          eius non praesentium voluptas odit ipsam quos officia, id nulla rem!
+          Quae nulla molestias atque deserunt, fugit illo facere hic ipsam quis
+          adipisci eaque consectetur iusto ducimus reprehenderit natus
+          perspiciatis in dolore aliquid? Suscipit repudiandae numquam
+          praesentium voluptate fuga, ab officiis? Exercitationem iure ullam
+          quia magni veniam aliquid voluptate!
+        </div>
 
-      <footer></footer>
+        <div className={styles.about_metrics1}></div>
+      </div>
+      <CopyFooter></CopyFooter>
     </div>
   );
 };
