8c5c24b0bd803ed15f1c0f0fd5cf7dce0d57842b
Prashant <prashantrahul141@protonmail.com>
2022-10-28 22:58:30 +0530

    added navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,25 @@
+import styles from '../styles/navbar.module.css';
+import Link from 'next/link';
+
+const NavBar = () => {
+  return (
+    <nav className={styles.navigationBar}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/blogs">Blogs</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="blogs/create">Create Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
