e7e03abda7f1ac30b0c6a2774bdec753afdb074c
Prashant <prashantrahul141@protonmail.com>
2022-10-30 23:58:03 +0530

    changed styling for /noBLogsfound

--- components/blogs/noblogsfound.tsx
+++ components/blogs/noblogsfound.tsx
@@ -5,7 +5,7 @@ import styles from '../../styles/blogroutes_styles/blogs.module.css';
 const NoBlogsFound: FC = () => {
   return (
     <div className={styles.no_blogs_found_main}>
-      <p className={styles.no_blogs_found_text}>No blogs found.</p>
+      <p className={styles.no_blogs_found_text}>No blogs found...</p>
       <Link href={'/blogs/create'}>
         <button className={styles.no_blogs_found_btn}>Create Blog!</button>
       </Link>
